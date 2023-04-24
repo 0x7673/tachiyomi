b304d20e0a63d6c44c3d1015598dc84ddb318840
0x7673 <123292609+0x7673@users.noreply.github.com>
2023-04-25 00:28:31 +0530

    Update category of library items when category is deleted

--- domain/src/main/java/tachiyomi/domain/category/interactor/DeleteCategory.kt
+++ domain/src/main/java/tachiyomi/domain/category/interactor/DeleteCategory.kt
@@ -5,12 +5,33 @@ import tachiyomi.core.util.lang.withNonCancellableContext
 import tachiyomi.core.util.system.logcat
 import tachiyomi.domain.category.model.CategoryUpdate
 import tachiyomi.domain.category.repository.CategoryRepository
+import tachiyomi.domain.manga.repository.MangaRepository
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 
 class DeleteCategory(
     private val categoryRepository: CategoryRepository,
+    private val mangaRepository: MangaRepository,
+    private val getCategories: GetCategories = Injekt.get(),
+    private val setMangaCategories: SetMangaCategories = Injekt.get(),
 ) {
 
     suspend fun await(categoryId: Long) = withNonCancellableContext {
+        /* Remove deleted category from library items */
+        val libraryMangaList = mangaRepository.getLibraryManga()
+        for (libraryManga in libraryMangaList) {
+            if (libraryManga.category != categoryId) {
+                continue
+            }
+            val manga = libraryManga.manga
+            val categoryIds = getCategories.await(manga.id)
+                .map { it.id }
+                .subtract(setOf(categoryId))
+                .toList()
+
+            setMangaCategories.await(manga.id, categoryIds)
+        }
+
         try {
             categoryRepository.delete(categoryId)
         } catch (e: Exception) {
